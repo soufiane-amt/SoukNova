d9623410f2525b95fcc7495a65e41ac803b33a53
Soufiane Amajat <soufianeamt11@gmail.com>
2025-05-28 15:31:52 +0100

    style: styles backup

--- frontend/src/app/features/auth/components/AuthSidePanel.tsx
+++ frontend/src/app/features/auth/components/AuthSidePanel.tsx
@@ -1,18 +1,17 @@
-'use client'
+'use client';
 const imageUrl = '/images/backgrounds/auth_back_image.png';
 
 export default function AuthSidePanel() {
   return (
-    <div className="w-1/2  h-screen relative">
+    <div className="w-full md:w-[50%] h-[40vh] md:h-screen relative bg-[#F3F5F7]  flex justify-center">
       <h1 className="absolute top-4 left-1/2 font-bold text-xl">3legant.</h1>
-      <img className="w-full h-full object-cover z-[-1]" src={imageUrl} alt="Side visual" />
+      <div className="w-full h-full [@media(min-width:980px)]:w-[80%] [@media(min-width:980px)]:h-[80%]">
+        <img
+          className=" "
+          src={imageUrl}
+          alt="Side visual"
+        />
+      </div>
     </div>
   );
 }
-
-
-{/* <div className="w-1/2 h-screen relative">
-  <div className="absolute inset-0 bg-black bg-opacity-40 z-0" />
-  <h1 className="absolute top-4 left-4 text-white text-2xl font-bold z-10">3legant</h1>
-  <img src={imageUrl} alt="Side visual" className="w-full h-full object-cover z-[-1]" />
-</div> */}
