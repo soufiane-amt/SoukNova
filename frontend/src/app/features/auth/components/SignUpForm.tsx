95bba9792f9027091063266257bc64e3216c4c58
Soufiane Amajat <soufianeamt11@gmail.com>
2025-05-29 13:42:37 +0100

    feat: responsivness checked

--- frontend/src/app/features/auth/components/SignUpForm.tsx
+++ frontend/src/app/features/auth/components/SignUpForm.tsx
@@ -17,40 +17,42 @@ export default function SignUpForm() {
   };
 
   return (
-    <div className=" m-auto w-full max-w-sm font-sans ">
-      <div className="my-8">
-        <div className="my-6">
-          <h1 className="text-[#141718] text-4xl font-bold ">Sign up</h1>
+    <div className=" m-auto w-full max-w-sm font-sans md:mx-10 px-5">
+      <div className=" md:my-8 my-4">
+        <div className="md:my-6 my:3">
+          <h1 className="text-[#141718] text-2xl md:text-4xl font-bold ">
+            Sign up
+          </h1>
         </div>
         <div>
-          <label className="text-[#6C7275] text-base ">
+          <label className="text-[#6C7275] text-sm md:text-base ">
             Already have an account?&nbsp;
             <a className="font-bold text-black">Sign in</a>
           </label>
         </div>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-8">
+      <form className="" onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4 md:mb-8">
           <input
-            className="w-full pb-4 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-base"
+            className="w-full pb-2 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-xs md:text-base"
             {...register('firstName')}
             placeholder="First Name"
           />
           <p className="text-red-500 text-xs">{errors.firstName?.message}</p>
         </div>
 
-        <div className="mb-8">
+        <div className="mb-4 md:mb-8">
           <input
-            className="w-full pb-4 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-base"
+            className="w-full pb-2 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-xs md:text-base"
             {...register('lastName')}
             placeholder="Last Name"
           />
           <p className="text-red-500 text-xs">{errors.lastName?.message}</p>
         </div>
 
-        <div className="mb-8">
+        <div className="mb-4 md:mb-8">
           <input
-            className="w-full pb-4 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-base"
+            className="w-full pb-2 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-xs md:text-base"
             {...register('email')}
             type="email"
             placeholder="Email address"
@@ -58,9 +60,9 @@ export default function SignUpForm() {
           <p className="text-red-500 text-xs">{errors.email?.message}</p>
         </div>
 
-        <div className="mb-8">
+        <div className="mb-4 md:mb-8">
           <input
-            className="w-full pb-4 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-base"
+            className="w-full pb-2 border-b border-b-[#E8ECEF] focus:outline-none text-sm text-[#6C7275] text-xs md:text-base"
             {...register('password')}
             type="password"
             placeholder="Password"
@@ -68,21 +70,23 @@ export default function SignUpForm() {
           <p className="text-red-500 text-xs">{errors.password?.message}</p>
         </div>
 
-        <div className="flex items-center my-8">
+        <div className="flex items-center my-4 md:my-8">
           <input type="checkbox" className="w-4 h-4 mr-3" />
-          <label className="text-[#6C7275] text-base">
+          <label className="text-[#6C7275] text-xs md:text-base">
             I agree with&nbsp;
             <a className="font-bold text-black">Privacy Policy</a>
             &nbsp;and&nbsp;
             <a className="font-bold text-black">Terms of Use</a>
           </label>
         </div>
-        <button
-          className="w-full py-3 bg-[#141718] text-white rounded-md hover:bg-[#47555a] cursor-pointer transition-colors duration-300"
-          type="submit"
-        >
-          Sign Up
-        </button>
+        <div className="flex justify-center">
+          <button
+            className="w-[90%] md:w-full py-3 bg-[#141718] text-white rounded-md hover:bg-[#47555a] cursor-pointer transition-colors duration-300"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </div>
       </form>
       {/* #47555a */}
     </div>
