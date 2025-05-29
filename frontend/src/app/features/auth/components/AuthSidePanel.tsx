abb4831271ad861c6c89243595356e68960b5d82
Soufiane Amajat <soufianeamt11@gmail.com>
2025-05-29 15:27:20 +0100

    feat : add signIn UI and  configure Zod

--- frontend/src/app/features/auth/components/AuthSidePanel.tsx
+++ frontend/src/app/features/auth/components/AuthSidePanel.tsx
@@ -7,7 +7,7 @@ const imageUrl = '/images/backgrounds/auth_back_image.png';
 export default function AuthSidePanel() {
   return (
     <div className="w-screen md:w-[50%] md:h-screen bg-[#F3F5F7] z-[0] relative">
-      <h1 className=" top-4 left-1/2 font-bold text-xl absolute">3legant.</h1>
+      <h1 className=" top-4 left-[40%] font-bold text-xl absolute">3legant.</h1>
       <div className="w-full flex justify-center items-center">
         <Image
           className="w-[200px] md:w-[600px] object-cover"
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 import AuthSidePanel from './features/auth/components/AuthSidePanel';
+import SignInForm from './features/auth/components/SignInForm';
 import SignUpForm from './features/auth/components/SignUpForm';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col md:flex-row">
       <AuthSidePanel />
-      <SignUpForm />
+      <SignInForm />
     </div>
   );
 }
 
-    // <div className="flex min-h-screen flex-col items-center justify-center md:flex-row ">
