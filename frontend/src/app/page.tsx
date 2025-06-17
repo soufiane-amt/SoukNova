fe3fdef8ce62d64b74222fed1cd2661b8a7a14a6
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-17 17:49:29 +0100

    feat: advancing on homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,17 @@
 import NavBar from '@/components/Navbar';
+import { ProductCard } from '@/components/Product/ProductCard';
+import RatingStars from '@/components/RatingStars';
 import CustomCarousel from '@/home/components/CustomCarousel';
 import { PromoSection } from '@/home/components/PromoSection';
+import React from 'react';
+
+const imageUrl = '/images/content/product1.png';
 
 export default function main() {
   return (
     <>
-      <NavBar />
-      <PromoSection/>
+      {/* <PromoSection /> */}
+      <ProductCard productName='Loveseat Sofa' currentPrice={199.00} originalPrice={499.00} rating={5} image={imageUrl}/>
     </>
   );
 }
--- frontend/src/home/components/PromoCardLarge.tsx
+++ frontend/src/home/components/PromoCardLarge.tsx
@@ -5,19 +5,26 @@ interface PromoCardProps {
   image: string;
 }
 
-
 export const PromoCardLarge: React.FC<PromoCardProps> = ({ title, image }) => {
-    return (
-        <div className="relative mt-5 mr-5">
-            <div>
-                <Image src={image} alt="Description" width={500} height={300} />
-            </div>
-            <div className="absolute top-7 left-7 ">
-                <h5 className='text-4xl'>{title}</h5>
-                <div className='mt-3'>
-                    <button className='text-lg underline cursor-pointer'>Shop now</button>
-                </div>
-            </div>
+  return (
+    <div className="mt-5 md:mr-5 flex flex-col items-center">
+      <div className="relative max-w-[500px] w-full">
+        <Image
+          src={image}
+          alt={title}
+          width={500}
+          height={300}
+          className="w-full h-auto object-cover"
+        />
+        <div className="absolute top-7 left-7">
+          <h5 className="md:text-4xl text-2xl">{title}</h5>
+          <div className="mt-3">
+            <button className="md:text-lg text-md underline cursor-pointer">
+              Shop now
+            </button>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
