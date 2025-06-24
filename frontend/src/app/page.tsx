a23286caa17d7284ba8725ae3c4bd6fa31690b24
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-24 15:53:45 +0100

    fix : responsivness

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from '@/components/Navbar';
 import { ProductCard } from '@/components/Product/ProductCard';
 import RatingStars from '@/components/RatingStars';
+import BenefitsDisplay from '@/home/components/BenefitsDisplay';
 import BenefitCard from '@/home/components/BenifitCard';
 import CustomCarousel from '@/home/components/CustomCarousel';
 import NewProduct from '@/home/components/NewProduct';
@@ -11,6 +12,7 @@ import React from 'react';
 export default function main() {
   return (
     <div>
+      <BenefitsDisplay/>
     </div>
   );
 }
--- frontend/src/home/components/BenefitsDisplay.tsx
+++ frontend/src/home/components/BenefitsDisplay.tsx
@@ -8,38 +8,28 @@ function BenefitsDisplay() {
     {
       icon: 'shipping',
       title: 'Free Shipping',
-      description:
-        'Order above $200',
+      description: 'Order above $200',
     },
     {
       icon: 'money',
       title: 'Money-back',
-      description:
-        '30 days guarantee',
+      description: '30 days guarantee',
     },
     {
       icon: 'security',
       title: 'Secure Payments',
-      description:
-        'Secured by Stripe',
+      description: 'Secured by Stripe',
     },
     {
       icon: 'support',
       title: '24/7 Support',
-      description:
-        'Phone and Email support',
+      description: 'Phone and Email support',
     },
   ];
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Grid container spacing={2}>
+    <Box>
+      <Grid className="flex justify-center items-center" container spacing={2}>
         {benefits.map((benefit, index) => (
           <BenefitCard
             key={index}
