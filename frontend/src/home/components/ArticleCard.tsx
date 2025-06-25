876b287214a216463f883c68470d618b281bb67c
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-25 15:39:50 +0100

    feat: add ArticleSection

---
+++ frontend/src/home/components/ArticleCard.tsx
@@ -0,0 +1,23 @@
+import CustomButton from '@/components/CustomButton';
+import { Typography } from '@mui/material';
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+
+interface ArticleCardProps {
+  title: string;
+  image: string;
+}
+
+export function ArticleCard({ title, image }: ArticleCardProps) {
+  return (
+    <div className='mb-7'>
+      <div>
+        <Image src={image} height={325} width={357} alt={title} />
+      </div>
+      <Typography sx={{ fontFamily: 'Poppins, sans-serif', paddingTop: '15px', fontWeight: 'bold' }}>
+        {title}
+      </Typography>
+      <CustomButton label="Read More" />
+    </div>
+  );
+}
---
+++ frontend/src/home/components/ArticleSection.tsx
@@ -0,0 +1,37 @@
+import { Typography } from '@mui/material';
+import { ArticleCard } from './ArticleCard';
+import CustomButton from '@/components/CustomButton';
+
+export function ArticleSection() {
+  const articals = [
+    {
+      title: '7 ways to decor your home',
+      image: '/images/home/article/article1.png',
+    },
+    {
+      title: 'Kitchen organization',
+      image: '/images/home/article/article2.png',
+    },
+    {
+      title: 'Decor your bedroom',
+      image: '/images/home/article/article3.png',
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-10">
+      <div className="flex justify-between w-full max-w-6xl items-center">
+        <Typography variant="h3" sx={{ fontWeight: 'semibold' }}>
+          Articles
+        </Typography>
+        <CustomButton label="More articles" />
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-10 items-center justify-center">
+        {articals.map((item, index) => (
+          <ArticleCard key={index} title={item.title} image={item.image} />
+        ))}
+      </div>
+    </div>
+  );
+}
