dfac423236c009094925426e6cc942fe9a7a0aa9
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-25 13:38:57 +0100

    feat: build PromoSection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,11 @@
-import NavBar from '@/components/Navbar';
-import { ProductCard } from '@/components/Product/ProductCard';
-import RatingStars from '@/components/RatingStars';
-import BenefitsDisplay from '@/home/components/BenefitsDisplay';
-import BenefitCard from '@/home/components/BenifitCard';
-import CustomCarousel from '@/home/components/CustomCarousel';
-import NewProduct from '@/home/components/NewProduct';
-import { PromoSection } from '@/home/components/PromoSection';
+import { PromoHighlightSection } from '@/home/components/PromoHighlightSection';
 import React from 'react';
 
 
 export default function main() {
   return (
     <div>
-      <BenefitsDisplay/>
+      <PromoHighlightSection/>
     </div>
   );
 }
---
+++ frontend/src/home/components/PromoHighlightSection.tsx
@@ -0,0 +1,75 @@
+import { Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+
+const promoHighlightImage = '/images/home/fourniture.png';
+
+export function PromoHighlightSection() {
+  return (
+    <div className="w-full md:flex md:h-[532px]">
+      <div className="flex-1">
+        <Image
+          src={promoHighlightImage}
+          alt="Highlight promotion"
+          width={600}
+          height={400}
+          className="w-full h-[367px] md:h-[532px]"
+        />
+      </div>
+      <div className="flex  items-center bg-[#F3F5F7] flex-1 ">
+        <div className=" px-15 py-12 w-2/3">
+          <Typography
+            variant="subtitle1"
+            component="div"
+            sx={{ fontFamily: 'Inter', fontWeight: 'bold' }}
+            gutterBottom
+            color="#377DFF"
+          >
+            SALE UP TO 35% OFF
+          </Typography>
+          <Typography
+            variant="h4"
+            component="div"
+            gutterBottom
+            color="main.primary"
+            style={{ fontWeight: 'bold' }}
+          >
+            HUNDREDS of New lower prices!
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              mt: 2,
+              color: 'text.secondary',
+              lineHeight: 1.6,
+            }}
+          >
+            It’s more affordable than ever to give every room in your home a
+            stylish makeover
+          </Typography>
+          <Button
+            variant="outlined"
+            disableElevation
+            disableRipple
+            disableFocusRipple
+            sx={{
+              all: 'unset',
+              cursor: 'pointer',
+              display: 'inline-flex',
+              alignItems: 'center',
+              color: 'inherit',
+              textDecoration: 'underline',
+              paddingBlock: '10px',
+              justifyItems: 'center',
+              marginTop: '15px',
+            }}
+            endIcon={<ArrowForwardIcon sx={{ width: 18 }} />}
+          >
+            Shop Now
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
