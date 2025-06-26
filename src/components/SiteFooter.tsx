469e267c07c680f596edd3f71408411115403ed1
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-26 15:50:30 +0100

    feat: add SiteFooter

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -8,7 +8,7 @@ import {
   Stack,
   Typography,
 } from '@mui/material';
-import { Facebook, Instagram, Youtube } from 'lucide-react';
+import { Facebook, Instagram, Pointer, Youtube } from 'lucide-react';
 // import InstagramIcon from '@mui/icons-material/Instagram';
 // import FacebookIcon from '@mui/icons-material/Facebook';
 // import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -19,7 +19,7 @@ const socialLinks = [
   { icon: <Youtube />, url: 'https://youtube.com/', label: 'YouTube' },
 ];
 
-const navItems = ['Home', 'Shop', 'Product', 'blog', 'Contact Us'];
+const navItems = ['Home', 'Shop', 'Product', 'blog', 'Contact'];
 
 function SocialIcons() {
   return (
@@ -43,19 +43,24 @@ function SocialIcons() {
 
 export function SiteFooter() {
   return (
-    <footer className="bg-[#232627] text-white py-13 px-10">
-      <div className="flex flex-col items-center justify-center ">
-        <div>
-          <h1 className="text-xl font-bold ">3legant.</h1>
-        </div>
-        <div className="text-[#E8ECEF] text-[14px] py-5">
-          <a href="#">Gift & Decoration Store</a>
+    <footer className="bg-[#232627] text-white p-15">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-between">
+        <div className="flex  flex-col md:flex-row items-center justify-center">
+          <div className=" flex flex-col items-center md:flex-row">
+            <h1 className="text-xl font-medium text-[24px] leading-[24px] font-poppins">
+              3legant.
+            </h1>
+            <div className=" h-4 md:h-7 w-7 md:pr-10 md:border-r md:border-b-0 border-b border-[#E8ECEF]"></div>
+          </div>
+          <div className="text-[#E8ECEF] text-[14px] py-5 md:pl-10">
+            <a href="#">Gift & Decoration Store</a>
+          </div>
         </div>
         <div className="md:inline text-[#E8ECEF] ">
           <List
             sx={{
               display: 'flex',
-              flexDirection: 'column',
+              flexDirection: { xs: 'column', sm: 'row' },
               alignItems: 'center',
             }}
           >
@@ -68,6 +73,7 @@ export function SiteFooter() {
                     textAlign: 'center',
                     paddingBlock: '7px',
                   }}
+                  sx={{ cursor: 'pointer' }}
                 />
               </ListItem>
             ))}
@@ -76,35 +82,36 @@ export function SiteFooter() {
       </div>
 
       <div
-        className='pt-5'
+        className="pt-5 md:pt-0 md:flex md:flex-row-reverse md:justify-between md:items-center"
         style={{
-          borderTop: '0.5px solid #ddd',
+          borderTop: '0.5px solid #7d6969',
           width: '100%',
-          margin: '10px 0',
         }}
       >
         <SocialIcons />
-        <Stack
-          direction="row"
-          spacing={5}
-          justifyContent="center"
-          mb={1}
-          fontSize={12}
-          paddingBlock={3}
-        >
-          <Link href="/privacy" underline="hover" color="inherit">
-            Privacy Policy
-          </Link>
-          <Link href="/terms" underline="hover" color="inherit">
-            Terms of Use
-          </Link>
-        </Stack>
-        <Typography
-          variant="body2"
-          sx={{ fontSize: '12px', color: '#E8ECEF', textAlign: 'center' }}
-        >
-          Copyright © {new Date().getFullYear()} Your Name. All rights reserved.
-        </Typography>
+        <div className="md:flex md:flex-row-reverse md:items-center md:grid-cols-4 md:gap-5">
+          <Stack
+            direction="row"
+            spacing={5}
+            justifyContent="center"
+            fontSize={12}
+            paddingBlock={3}
+          >
+            <Link href="/privacy" underline="hover" color="inherit">
+              Privacy Policy
+            </Link>
+            <Link href="/terms" underline="hover" color="inherit">
+              Terms of Use
+            </Link>
+          </Stack>
+          <Typography
+            variant="body2"
+            sx={{ fontSize: '12px', color: '#E8ECEF', textAlign: 'center' }}
+          >
+            Copyright © {new Date().getFullYear()} Your Name. All rights
+            reserved.
+          </Typography>
+        </div>
       </div>
     </footer>
   );
