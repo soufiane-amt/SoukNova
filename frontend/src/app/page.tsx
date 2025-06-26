f17c5259a74cb26329074e5f13fb2a5a8d8354bb
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-26 12:05:33 +0100

    feat: add NewsLetterSub

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
 import { ArticleCard } from '@/home/components/ArticleCard';
 import { ArticleSection } from '@/home/components/ArticleSection';
+import { NewsLetterSub } from '@/home/components/NewsLetterSub';
 import { PromoHighlightSection } from '@/home/components/PromoHighlightSection';
 import React from 'react';
 
 export default function main() {
-  return <ArticleSection />;
+  return <NewsLetterSub />;
 }
