8d20f5267f4713fb9a644d09cf61c36852cac189
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-28 15:27:28 +0100

    fix: the styling of promoSection

---
+++ frontend/src/app/home.tsx
@@ -0,0 +1,38 @@
+import { SiteFooter } from '@/components/SiteFooter';
+import { ArticleSection } from '@/home/components/ArticleSection';
+import BenefitsDisplay from '@/home/components/BenefitsDisplay';
+import CustomCarousel from '@/home/components/CustomCarousel';
+import NewArrivalSection from '@/home/components/NewArrivalSection';
+import { NewsLetterSub } from '@/home/components/NewsLetterSub';
+import { PromoHighlightSection } from '@/home/components/PromoHighlightSection';
+import { PromoSection } from '@/home/components/PromoSection';
+
+function HomePage() {
+  return (
+    <div>
+      <div className="px-6 md:px-12 lg:px-24 max-w-screen-2xl mx-auto">
+        <CustomCarousel />
+        <PromoSection />
+        {/* <div className="my-10">
+          <NewArrivalSection />
+        </div>
+        <div className="my-10">
+          <BenefitsDisplay />
+        </div> */}
+      </div>
+
+      {/* <div className="w-full mt-10">
+        <PromoHighlightSection />
+      </div>
+      <div className="px-4 md:px-12 lg:px-24 max-w-screen-2xl mx-auto my-12">
+        <ArticleSection />
+      </div>
+      <div className="w-full">
+        <NewsLetterSub />
+        <SiteFooter />
+      </div> */}
+    </div>
+  );
+}
+
+export default HomePage;
