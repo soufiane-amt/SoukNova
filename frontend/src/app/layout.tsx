1f226e66f3d1a9ceb8c50a2e973650bacb76e5e9
Soufiane Amajat <soufianeamt11@gmail.com>
2025-07-10 12:24:50 +0100

    fix : posting signin info and  fixing the schema issues

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '../globals.css';
 import NavBar from '../components/layout/NavBar/Navbar';
+import ConditionalNavBar from '../components/layout/NavBar/ConditionalNavBar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
+        <ConditionalNavBar />
         {children}
       </body>
     </html>
