cb8495a26543e6bb8ef1db32e4591ba8c6d7bbb4
Soufiane Amajat <soufianeamt11@gmail.com>
2025-08-11 13:35:15 +0100

    feat : finishing shop page

--- frontend/src/app/shop/components/CategoryFilter.tsx
+++ frontend/src/app/shop/components/CategoryFilter.tsx
@@ -1,7 +1,5 @@
 'use client';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import * as React from 'react';
 
 import { useState } from 'react';
 import {
@@ -10,7 +8,6 @@ import {
   DisclosurePanel,
   Menu,
   MenuButton,
-  MenuItem,
   MenuItems,
 } from '@headlessui/react';
 import {
@@ -20,12 +17,11 @@ import {
   PlusIcon,
 } from '@heroicons/react/24/outline';
 import { Squares2X2IconButton } from '../../../components/ui/squares/Squares2X2Icon';
-import { Squares1X1IconVerticalButton } from '../../../components/ui/squares/Squares1X1IconVertical';
-import { Squares1X1IconHorizontalButton } from '../../../components/ui/squares/Squares1X1IconHorizontal';
 import { Squares3X3IconButton } from '../../../components/ui/squares/Squares3X3Icon';
 import { ProductCard } from '../../../components/ui/ProductCard';
 import { arrivals } from '../../../constants/arrivals';
 import { Grid } from '@mui/material';
+import { FormControl, MenuItem, Select } from '@mui/material';
 
 const sortOptions = [
   { name: 'Most Popular', href: '#', current: true },
@@ -34,44 +30,43 @@ const sortOptions = [
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ];
-const filters = [
-  {
-    id: 'price',
-    name: 'PRICE',
-    options: [
-      {
-        value: { minPrice: 0, maxPrice: Infinity },
-        label: 'All Price',
-        checked: false,
-      },
-      {
-        value: { minPrice: 0, maxPrice: 99.99 },
-        label: '$0.00 - 99.99',
-        checked: false,
-      },
-      {
-        value: { minPrice: 100, maxPrice: 199.99 },
-        label: '$100.00 - 199.99',
-        checked: true,
-      },
-      {
-        value: { minPrice: 200, maxPrice: 299.99 },
-        label: '$200.00 - 299.99',
-        checked: false,
-      },
-      {
-        value: { minPrice: 300, maxPrice: 399.99 },
-        label: '$300.00 - 399.99',
-        checked: false,
-      },
-      {
-        value: { minPrice: 400, maxPrice: Infinity },
-        label: '$400.00+',
-        checked: false,
-      },
-    ],
-  },
-];
+const priceFilter = {
+  id: 'price',
+  name: 'PRICE',
+  options: [
+    {
+      value: { minPrice: 0, maxPrice: Infinity },
+      label: 'All Price',
+      checked: false,
+    },
+    {
+      value: { minPrice: 0, maxPrice: 99.99 },
+      label: '$0.00 - 99.99',
+      checked: false,
+    },
+    {
+      value: { minPrice: 100, maxPrice: 199.99 },
+      label: '$100.00 - 199.99',
+      checked: true,
+    },
+    {
+      value: { minPrice: 200, maxPrice: 299.99 },
+      label: '$200.00 - 299.99',
+      checked: false,
+    },
+    {
+      value: { minPrice: 300, maxPrice: 399.99 },
+      label: '$300.00 - 399.99',
+      checked: false,
+    },
+    {
+      value: { minPrice: 400, maxPrice: Infinity },
+      label: '$400.00+',
+      checked: false,
+    },
+  ],
+};
+
 const subCategories = [
   { name: 'All Rooms', href: '#' },
   { name: 'Living Room', href: '#' },
@@ -81,14 +76,65 @@ const subCategories = [
   { name: 'Dinning', href: '#' },
   { name: 'Outdoor', href: '#' },
 ];
-
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
+function BasicDropdownFilter() {
+  const [selectedCategory, setSelectedCategory] = React.useState(
+    subCategories[0].name,
+  );
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <Select
+        labelId="category-select-label"
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+        sx={{ height: 45, borderRadius: 2 }}
+      >
+        {subCategories.map((el) => (
+          <MenuItem key={el.name} value={el.name}>
+            {el.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+function BasicDropdownPrice() {
+  const [selectedPrice, setSelectedPrice] = React.useState(
+    priceFilter.options[0].label,
+  );
+
+  const handlePriceChange = (event) => {
+    setSelectedPrice(event.target.value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <Select
+        labelId="price-select-label"
+        value={selectedPrice}
+        onChange={handlePriceChange}
+        sx={{ height: 45, borderRadius: 2 }}
+      >
+        {priceFilter.options.map((el, index) => (
+          <MenuItem key={index} value={el.label}>
+            {el.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
 export default function CategoryFilter() {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
-  const [selectedShape, setSelectedShape] = useState(1);
+  const [selectedShape, setSelectedShape] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState(1);
   const [selectedPriceIndex, setSelectedPriceIndex] = useState(0);
 
@@ -96,31 +142,34 @@ export default function CategoryFilter() {
     setSelectedShape(index);
   };
 
-  const handleSelectCategory = () => {};
-
   return (
-    <div className="bg-white">
       <div>
         <main className="mx-auto max-6xl p6-4 sm:px-6 lg:px-8">
-          <div className="flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
-            <div className="flex items-center space-x-2 text-gray-800 text-lg font-semibold">
-              <svg
-                width="20"
-                height="18"
-                viewBox="0 0 20 18"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 4H4M4 4C4 5.65685 5.34315 7 7 7C8.65685 7 10 5.65685 10 4C10 2.34315 8.65685 1 7 1C5.34315 1 4 2.34315 4 4ZM1 14H7M16 14H19M16 14C16 15.6569 14.6569 17 13 17C11.3431 17 10 15.6569 10 14C10 12.3431 11.3431 11 13 11C14.6569 11 16 12.3431 16 14ZM13 4H19"
-                  stroke="#141718"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  fill="white"
-                />
-              </svg>
-              <span>Filter</span>
-            </div>
+          <div className="flex items-baseline justify-between  pt-24 pb-6">
+            {selectedShape === 1 ? (
+              <div className="flex justify-between gap-4 w-1/3">
+                <BasicDropdownFilter />
+                <BasicDropdownPrice />
+              </div>
+            ) : (
+              <div className='flex items-center space-x-2 '>
+                <svg
+                  width="15"
+                  height="13"
+                  viewBox="0 0 20 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 4H4M4 4C4 5.65685 5.34315 7 7 7C8.65685 7 10 5.65685 10 4C10 2.34315 8.65685 1 7 1C5.34315 1 4 2.34315 4 4ZM1 14H7M16 14H19M16 14C16 15.6569 14.6569 17 13 17C11.3431 17 10 15.6569 10 14C10 12.3431 11.3431 11 13 11C14.6569 11 16 12.3431 16 14ZM13 4H19"
+                    stroke="#141718"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                  />
+                </svg>
+                <h2 className='font-bold text-center'>Filter</h2>
+              </div>
+            )}
 
             <div className="flex items-center justify-center">
               <Menu as="div" className="relative inline-block text-left mr-10">
@@ -162,18 +211,8 @@ export default function CategoryFilter() {
                 selectedShape={selectedShape}
                 handleClick={handleSelectShape}
               />
-              <Squares1X1IconVerticalButton
-                index={2}
-                selectedShape={selectedShape}
-                handleClick={handleSelectShape}
-              />
               <Squares2X2IconButton
                 index={1}
-                selectedShape={selectedShape}
-                handleClick={handleSelectShape}
-              />
-              <Squares1X1IconHorizontalButton
-                index={3}
                 selectedShape={selectedShape}
                 handleClick={handleSelectShape}
               />
@@ -192,106 +231,104 @@ export default function CategoryFilter() {
             <h2 id="products-heading" className="sr-only">
               Products
             </h2>
-
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-              <form className="hidden lg:block">
-                <h3 className="sr-only">Categories</h3>
-                <ul
-                  role="list"
-                  className="space-y-4 pb-6 text-sm font-medium text-gray-900"
-                >
-                  {subCategories.map((category, index) => (
-                    <li key={category.name}>
-                      <a
-                        className={`${inter.className} text-[#807E7E] text-sm ${
-                          selectedCategory == index
-                            ? 'border-b text-black font-bold'
-                            : ''
-                        }`}
-                        href={category.href}
-                        onClick={() => setSelectedCategory(index)}
-                      >
-                        {category.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
+              {selectedShape === 0 && (
+                <form className="hidden lg:block">
+                  <h2 className='mb-5 font-bold text-md'>CATEGORIES</h2>
+                  <ul
+                    role="list"
+                    className="space-y-4 pb-6 text-sm font-medium text-gray-900"
+                  >
+                    {subCategories.map((category, index) => (
+                      <li key={category.name}>
+                        <a
+                          className={`text-[#807E7E] text-sm ${
+                            selectedCategory == index
+                              ? 'border-b text-black font-bold'
+                              : ''
+                          }`}
+                          href={category.href}
+                          onClick={() => setSelectedCategory(index)}
+                        >
+                          {category.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
 
-                {filters.map((section) => (
-                  <Disclosure key={section.id} as="div" className="py-6">
-                    <h3 className="-my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                        <span className="font-bold text-gray-900">
-                          {section.name}
-                        </span>
-                        <span className="ml-6 flex items-center">
-                          <PlusIcon
-                            aria-hidden="true"
-                            className="size-5 group-data-open:hidden"
-                          />
-                          <MinusIcon
-                            aria-hidden="true"
-                            className="size-5 group-not-data-open:hidden"
-                          />
-                        </span>
-                      </DisclosureButton>
-                    </h3>
-                    <DisclosurePanel className="pt-6">
-                      <div className="space-y-4">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={optionIdx} className="flex justify-between">
-                            <label
-                              htmlFor={`filter-${section.id}-${optionIdx}`}
-                              className={`${inter.className} text-[#807E7E] text-sm font-medium`}
+                  {
+                    <Disclosure key={priceFilter.id} as="div" className="py-6">
+                      <h3 className="-my-3 flow-root">
+                        <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                          <span className="font-bold text-gray-900">
+                            {priceFilter.name}
+                          </span>
+                        </DisclosureButton>
+                      </h3>
+                      <div className="pt-6">
+                        <div className="space-y-4">
+                          {priceFilter.options.map((option, optionIdx) => (
+                            <div
+                              key={optionIdx}
+                              className="flex justify-between"
                             >
-                              {option.label}
-                            </label>
+                              <label
+                                htmlFor={`filter-${priceFilter.id}-${optionIdx}`}
+                                className={`text-[#807E7E] text-sm font-medium`}
+                              >
+                                {option.label}
+                              </label>
 
-                            <div className="flex h-5 shrink-0 items-center">
-                              <div className="group grid size-4 grid-cols-1">
-                                <input
-                                  defaultChecked={
-                                    selectedPriceIndex === optionIdx
-                                  }
-                                  id={`filter-${section.id}-${optionIdx}`}
-                                  name={`${section.id}[]`}
-                                  type="radio"
-                                  className="p-2 col-start-1 row-start-1 appearance-none rounded-sm border border-primary bg-white checked:border-black checked:bg-black indeterminate:border-black indeterminate:bg-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto "
-                                  onClick={() =>
-                                    setSelectedPriceIndex(optionIdx)
-                                  }
-                                />
-                                <svg
-                                  fill="none"
-                                  viewBox="0 0 14 14"
-                                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
-                                >
-                                  <path
-                                    d="M3 8L6 11L11 3.5"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-checked:opacity-100"
+                              <div className="flex h-5 shrink-0 items-center">
+                                <div className="group grid size-4 grid-cols-1">
+                                  <input
+                                    defaultChecked={
+                                      selectedPriceIndex === optionIdx
+                                    }
+                                    id={`filter-${priceFilter.id}-${optionIdx}`}
+                                    name={`${priceFilter.id}[]`}
+                                    type="radio"
+                                    className="p-2 col-start-1 row-start-1 appearance-none rounded-sm border border-primary bg-white checked:border-black checked:bg-black indeterminate:border-black indeterminate:bg-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto "
+                                    onClick={() =>
+                                      setSelectedPriceIndex(optionIdx)
+                                    }
                                   />
-                                  <path
-                                    d="M3 7H11"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-indeterminate:opacity-100"
-                                  />
-                                </svg>
+                                  <svg
+                                    fill="none"
+                                    viewBox="0 0 14 14"
+                                    className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                                  >
+                                    <path
+                                      d="M3 8L6 11L11 3.5"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="opacity-0 group-has-checked:opacity-100"
+                                    />
+                                    <path
+                                      d="M3 7H11"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="opacity-0 group-has-indeterminate:opacity-100"
+                                    />
+                                  </svg>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        ))}
+                          ))}
+                        </div>
                       </div>
-                    </DisclosurePanel>
-                  </Disclosure>
-                ))}
-              </form>
+                    </Disclosure>
+                  }
+                </form>
+              )}
 
-              <div className="lg:col-span-3">
+              <div
+                className={`${
+                  selectedShape === 0 ? 'lg:col-span-3' : 'lg:col-span-4'
+                }`}
+              >
                 <Grid
                   container
                   spacing={2}
@@ -320,7 +357,6 @@ export default function CategoryFilter() {
             </div>
           </section>
         </main>
-      </div>
     </div>
   );
 }
---
+++ frontend/src/app/shop/components/ShopShow.tsx
@@ -0,0 +1,61 @@
+import { Typography } from '@mui/material';
+import Image from 'next/image';
+
+const imageUrl = '/images/shop/shopPage.png';
+
+export default function ShopShow() {
+  return (
+    <div className="relative">
+      <Image
+        className="w-full"
+        src={imageUrl}
+        alt="Side visual"
+        width={600}
+        height={400}
+        priority
+      />
+      <div className="absolute top-1/3 left-2/5">
+        <div className='mb-5 '>
+          <ol className="flex justify-center align-center ">
+            <li>
+              <a href="#home" className="text-sm font-medium text-gray-500">
+                Home
+              </a>
+            </li>
+            <li className='self-center'>
+              <span className="text-gray-400">
+                <svg
+                  className="h-5 w-5 flex-shrink-0"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="text-sm font-medium text-gray-700 hover:text-gray-700"
+              >
+                Shop
+              </a>
+            </li>
+          </ol>
+        </div>
+        <div className="flex  justify-center text-center">
+          <div>
+            <h1 className="text-6xl font-medium mb-5">Shop Page</h1>
+            <Typography>Let’s design the place you always imagined.</Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
