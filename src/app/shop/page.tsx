ed2a0501e8e325f1e433c35f2aaa6baa58b00ab0
Soufiane Amajat <soufianeamt11@gmail.com>
2025-08-11 13:35:15 +0100

    feat : finishing shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import CategoryFilter from './components/CategoryFilter';
+import ShopShow from './components/ShopShow';
 
 export default function ShopPage() {
-  return <CategoryFilter/>;
+  return (
+    <div className="mx-20">
+      <ShopShow/>
+      <CategoryFilter />
+    </div>
+  );
 }
