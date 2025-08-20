17ddf583c7866db5a01c066ba66396bb403592db
Soufiane Amajat <soufianeamt11@gmail.com>
2025-08-20 15:45:09 +0100

    feat: contact us done

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -10,6 +10,8 @@ const promoHighlightImage = '/images/home/fourniture.png';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
+import BenefitsDisplay from '@/home/components/BenefitsDisplay';
+import { SiteFooter } from '../../components/layout/SiteFooter';
 
 const imageMap = '/images/contact/map.png';
 
@@ -54,11 +56,11 @@ function ContactsInfo() {
         Contact Us
       </p>
 
-      <div>
+      <div className="md:flex justify-between space-x-8">
         {contacts.map((contact, index) => (
           <div
             key={index}
-            className="bg-[var(--color-neutral-bg)] flex flex-col items-center text-center p-5 mb-5"
+            className="bg-[var(--color-neutral-bg)] flex flex-col  items-center text-center p-5 mb-5 flex-1"
           >
             <span>{contact.icon}</span>
             <Typography
@@ -80,8 +82,8 @@ function ContactsInfo() {
 
 function AboutUsCard() {
   return (
-    <div className="bg-[var(--color-neutral-bg)]">
-      <div>
+    <div className="bg-[var(--color-neutral-bg)] md:flex">
+      <div className="md:w-1/2">
         <Image
           src={promoHighlightImage}
           alt="Highlight promotion"
@@ -90,8 +92,10 @@ function AboutUsCard() {
           className="w-full h-[367px] md:h-[532px]"
         />
       </div>
-      <div className="mt-10 p-5">
-        <p className={`${poppins.className} mb-5 font-medium text-lg mb-3`}>
+      <div className="mt-10 p-5 md:w-1/2 md:p-15">
+        <p
+          className={`${poppins.className} mb-5 font-medium text-lg md:text-4xl  mb-3`}
+        >
           About Us
         </p>
 
@@ -108,7 +112,7 @@ function AboutUsCard() {
 
 function MapLoction() {
   return (
-    <div className="relative">
+    <div className="relative w-2/5">
       <Image
         src={imageMap}
         alt="Company location"
@@ -125,8 +129,8 @@ function MapLoction() {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M32.5 58.6654C41.5 58.6654 56.5 42.1269 56.5 29.0357C56.5 15.9445 45.7548 5.33203 32.5 5.33203C19.2452 5.33203 8.5 15.9445 8.5 29.0357C8.5 42.1269 23.5 58.6654 32.5 58.6654ZM32.5 37.332C36.9183 37.332 40.5 33.7503 40.5 29.332C40.5 24.9138 36.9183 21.332 32.5 21.332C28.0817 21.332 24.5 24.9138 24.5 29.332C24.5 33.7503 28.0817 37.332 32.5 37.332Z"
           fill="#141718"
         />
@@ -134,20 +138,20 @@ function MapLoction() {
         <path
           d="M40.5 29.6523V35.6523C40.5 37.8615 38.7091 39.6523 36.5 39.6523H28.5C26.2909 39.6523 24.5 37.8615 24.5 35.6523V29.6523"
           stroke="#FEFEFE"
-          stroke-width="1.5"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
         />
         <path
           d="M37.2688 19.6523H27.7313C25.9139 19.6523 24.2797 20.8971 23.6047 22.7954L22.8467 24.9272C22.6173 25.5725 22.4587 26.2631 22.626 26.9272C23.0224 28.5001 24.3083 29.6523 25.8334 29.6523C27.6743 29.6523 29.1667 27.9734 29.1667 25.9023C29.1667 27.9734 30.6591 29.6523 32.5 29.6523C34.341 29.6523 35.8334 27.9734 35.8334 25.9023C35.8334 27.9734 37.3257 29.6523 39.1667 29.6523C40.6918 29.6523 41.9777 28.5001 42.374 26.9272C42.5414 26.2631 42.3828 25.5725 42.1533 24.9272L41.3953 22.7954C40.7204 20.8971 39.0861 19.6523 37.2688 19.6523Z"
           stroke="#FEFEFE"
-          stroke-width="1.5"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
         />
         <path
           d="M29.5 36.6523C29.5 34.9955 30.8431 33.6523 32.5 33.6523C34.1569 33.6523 35.5 34.9955 35.5 36.6523V39.6523H29.5V36.6523Z"
           stroke="#FEFEFE"
-          stroke-width="1.5"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinejoin="round"
         />
       </svg>
     </div>
@@ -156,7 +160,7 @@ function MapLoction() {
 
 function MessageInput() {
   return (
-    <div>
+    <div className="my-5 w-3/5">
       <div>
         <div className="flex flex-col mb-8">
           <label className="font-bold text-[var(--color-primary)] text-xs mb-2">
@@ -182,12 +186,16 @@ function MessageInput() {
           </label>
           <input
             placeholder="Your message"
-            className="text-md border py-2 px-4 pb-35 rounded-md border-[#CBCBCB]"
+            className="text-md border py-2 px-4 pb-35 md:pb-20 rounded-md border-[#CBCBCB]"
           />
         </div>
       </div>
       <div>
-        <button className={`${poppins.className} bg-black text-white w-full py-2 rounded-md font-semibold`}>Send Message</button>
+        <button
+          className={`${poppins.className} bg-black text-white w-full py-2 rounded-md font-semibold`}
+        >
+          Send Message
+        </button>
       </div>
     </div>
   );
@@ -195,35 +203,43 @@ function MessageInput() {
 
 function ContactPage() {
   return (
-    <main className="mx-10">
-      {/* <div>
-        <motion.div
-          className="flex mt-4 space-x-2"
-          variants={containerVariants}
-          initial="hidden"
-          animate="visible"
-        >
-          <Traversal
-            items={[{ label: 'Home', href: '/' }, { label: 'Contact' }]}
-          />
-        </motion.div>
+    <main>
+      <div className="mx-10 md:mx-15 lg:mx-30">
+        <div>
+          <motion.div
+            className="flex mt-4 space-x-2"
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            <Traversal
+              items={[{ label: 'Home', href: '/' }, { label: 'Contact' }]}
+            />
+          </motion.div>
+        </div>
+        <div className="mb-10 space-y-6 md:w-2/3">
+          <p className="text-[54px] font-medium leading-[58px] tracking-[-1px] max-sm:text-[28px] max-sm:leading-[34px]">
+            We believe in sustainable decor. We’re passionate about life at
+            home.
+          </p>
+          <p className="font-inter leading-[26px]">
+            Our features timeless furniture, with natural fabrics, curved lines,
+            plenty of mirrors and classic design, which can be incorporated into
+            any decor project. The pieces enchant for their sobriety, to last
+            for generations, faithful to the shapes of each period, with a touch
+            of the present
+          </p>
+        </div>
+        <AboutUsCard />
+        <ContactsInfo />
+        <div className="md:flex md:space-x-10">
+          <MessageInput />
+          <MapLoction />
+        </div>
+        <BenefitsDisplay />
       </div>
-      <div className="mb-10">
-        <Typography variant="h4" sx={{ fontWeight: 500 }}>
-          We believe in sustainable decor. We’re passionate about life at home.
-        </Typography>
-        <Typography variant="body1" sx={{ marginTop: 3 }}>
-          Our features timeless furniture, with natural fabrics, curved lines,
-          plenty of mirrors and classic design, which can be incorporated into
-          any decor project. The pieces enchant for their sobriety, to last for
-          generations, faithful to the shapes of each period, with a touch of
-          the present
-        </Typography>
-      </div>
-      <AboutUsCard />
-      <ContactsInfo />
-      <MapLoction /> */}
-      <MessageInput />
+
+      <SiteFooter />
     </main>
   );
 }
