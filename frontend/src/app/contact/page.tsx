bad936675f23b7d0badcd11e0e85d5c2e0a97ec0
Soufiane Amajat <soufianeamt11@gmail.com>
2025-08-20 15:02:29 +0100

    feat : build parts of contact page

---
+++ frontend/src/app/contact/page.tsx
@@ -0,0 +1,231 @@
+'use client';
+
+import { Typography } from '@mui/material';
+import Traversal from '../../components/ui/Traversal';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import CustomButton from '../../components/ui/CustomButton';
+import { poppins } from '@/layout';
+const promoHighlightImage = '/images/home/fourniture.png';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
+
+const imageMap = '/images/contact/map.png';
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2, // Stagger all children
+    },
+  },
+};
+
+interface ContactData {
+  title: string;
+  value: string;
+  icon: React.ReactNode;
+}
+
+const contacts: ContactData[] = [
+  {
+    title: 'ADDRESS',
+    value: '234 Hai Trieu, Ho Chi Minh City, Viet Nam',
+    icon: <BusinessOutlinedIcon sx={{ fontSize: 30 }} />,
+  },
+  {
+    title: 'Contact Us',
+    value: '+84 234 567 890',
+    icon: <LocalPhoneOutlinedIcon sx={{ fontSize: 30 }} />,
+  },
+  {
+    title: 'EMAIL',
+    value: 'hello@souknova.com',
+    icon: <EmailOutlinedIcon sx={{ fontSize: 30 }} />,
+  },
+];
+
+function ContactsInfo() {
+  return (
+    <div className="mt-10 text-center">
+      <p className={`${poppins.className} mb-5 font-medium text-4xl`}>
+        Contact Us
+      </p>
+
+      <div>
+        {contacts.map((contact, index) => (
+          <div
+            key={index}
+            className="bg-[var(--color-neutral-bg)] flex flex-col items-center text-center p-5 mb-5"
+          >
+            <span>{contact.icon}</span>
+            <Typography
+              variant="body2"
+              className="font-bold text-[var(--color-primary)]"
+              sx={{ fontWeight: 600, fontSize: 15, marginBlock: 1 }}
+            >
+              {contact.title}
+            </Typography>
+            <Typography variant="body1" sx={{ fontWeight: 600 }}>
+              {contact.value}
+            </Typography>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function AboutUsCard() {
+  return (
+    <div className="bg-[var(--color-neutral-bg)]">
+      <div>
+        <Image
+          src={promoHighlightImage}
+          alt="Highlight promotion"
+          width={600}
+          height={400}
+          className="w-full h-[367px] md:h-[532px]"
+        />
+      </div>
+      <div className="mt-10 p-5">
+        <p className={`${poppins.className} mb-5 font-medium text-lg mb-3`}>
+          About Us
+        </p>
+
+        <Typography variant="body2" sx={{ marginBottom: 3 }}>
+          3legant is a gift & decorations store based in HCMC, Vietnam. Est
+          since 2019. Our customer service is always prepared to support you
+          24/7
+        </Typography>
+        <CustomButton label="Shop now" />
+      </div>
+    </div>
+  );
+}
+
+function MapLoction() {
+  return (
+    <div className="relative">
+      <Image
+        src={imageMap}
+        alt="Company location"
+        width={500}
+        height={500}
+        className="max-h-[422.4px] min-h-[422.4px] w-[548px] max-2xl:w-full max-sm:h-[326.5px]"
+      />
+      <svg
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        width="65"
+        height="64"
+        viewBox="0 0 65 64"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M32.5 58.6654C41.5 58.6654 56.5 42.1269 56.5 29.0357C56.5 15.9445 45.7548 5.33203 32.5 5.33203C19.2452 5.33203 8.5 15.9445 8.5 29.0357C8.5 42.1269 23.5 58.6654 32.5 58.6654ZM32.5 37.332C36.9183 37.332 40.5 33.7503 40.5 29.332C40.5 24.9138 36.9183 21.332 32.5 21.332C28.0817 21.332 24.5 24.9138 24.5 29.332C24.5 33.7503 28.0817 37.332 32.5 37.332Z"
+          fill="#141718"
+        />
+        <circle cx="32.5" cy="29.6523" r="20" fill="#141718" />
+        <path
+          d="M40.5 29.6523V35.6523C40.5 37.8615 38.7091 39.6523 36.5 39.6523H28.5C26.2909 39.6523 24.5 37.8615 24.5 35.6523V29.6523"
+          stroke="#FEFEFE"
+          stroke-width="1.5"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M37.2688 19.6523H27.7313C25.9139 19.6523 24.2797 20.8971 23.6047 22.7954L22.8467 24.9272C22.6173 25.5725 22.4587 26.2631 22.626 26.9272C23.0224 28.5001 24.3083 29.6523 25.8334 29.6523C27.6743 29.6523 29.1667 27.9734 29.1667 25.9023C29.1667 27.9734 30.6591 29.6523 32.5 29.6523C34.341 29.6523 35.8334 27.9734 35.8334 25.9023C35.8334 27.9734 37.3257 29.6523 39.1667 29.6523C40.6918 29.6523 41.9777 28.5001 42.374 26.9272C42.5414 26.2631 42.3828 25.5725 42.1533 24.9272L41.3953 22.7954C40.7204 20.8971 39.0861 19.6523 37.2688 19.6523Z"
+          stroke="#FEFEFE"
+          stroke-width="1.5"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M29.5 36.6523C29.5 34.9955 30.8431 33.6523 32.5 33.6523C34.1569 33.6523 35.5 34.9955 35.5 36.6523V39.6523H29.5V36.6523Z"
+          stroke="#FEFEFE"
+          stroke-width="1.5"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
+
+function MessageInput() {
+  return (
+    <div>
+      <div>
+        <div className="flex flex-col mb-8">
+          <label className="font-bold text-[var(--color-primary)] text-xs mb-2">
+            FIRST NAME
+          </label>
+          <input
+            placeholder="First name"
+            className={`text-md border py-2 px-4 rounded-md border border-[#CBCBCB] ${poppins.className}`}
+          />
+        </div>
+        <div className="flex flex-col mb-8">
+          <label className="font-bold text-[var(--color-primary)] text-xs mb-2">
+            SECOND NAME
+          </label>
+          <input
+            placeholder="Last name"
+            className="text-md border py-2 px-4 rounded-md border border-[#CBCBCB]"
+          />
+        </div>
+        <div className="flex flex-col mb-8">
+          <label className="font-bold text-[var(--color-primary)] text-xs mb-2">
+            MESSAGE
+          </label>
+          <input
+            placeholder="Your message"
+            className="text-md border py-2 px-4 pb-35 rounded-md border-[#CBCBCB]"
+          />
+        </div>
+      </div>
+      <div>
+        <button className={`${poppins.className} bg-black text-white w-full py-2 rounded-md font-semibold`}>Send Message</button>
+      </div>
+    </div>
+  );
+}
+
+function ContactPage() {
+  return (
+    <main className="mx-10">
+      {/* <div>
+        <motion.div
+          className="flex mt-4 space-x-2"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <Traversal
+            items={[{ label: 'Home', href: '/' }, { label: 'Contact' }]}
+          />
+        </motion.div>
+      </div>
+      <div className="mb-10">
+        <Typography variant="h4" sx={{ fontWeight: 500 }}>
+          We believe in sustainable decor. We’re passionate about life at home.
+        </Typography>
+        <Typography variant="body1" sx={{ marginTop: 3 }}>
+          Our features timeless furniture, with natural fabrics, curved lines,
+          plenty of mirrors and classic design, which can be incorporated into
+          any decor project. The pieces enchant for their sobriety, to last for
+          generations, faithful to the shapes of each period, with a touch of
+          the present
+        </Typography>
+      </div>
+      <AboutUsCard />
+      <ContactsInfo />
+      <MapLoction /> */}
+      <MessageInput />
+    </main>
+  );
+}
+
+export default ContactPage;
