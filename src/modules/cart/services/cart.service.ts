7fada0dbe487b4ea3b4c5582774062e1f1b22d2d
Soufiane Amajat <soufianeamt11@gmail.com>
2025-09-18 17:52:24 +0100

    feat : add cart updates

--- src/modules/cart/services/cart.service.ts
+++ src/modules/cart/services/cart.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CartItemFullProps, CartItemProps } from '../dto/cart.dto';
+
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vd2NqY21kY2ZpdG5uc3Fmb2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MTI3MTksImV4cCI6MjA0NjM4ODcxOX0.bx4a1dNx8g-BZX2KWceWBuRlPwAqgxhZ80i7L4K8M7Y';
 
 @Injectable()
 export class CartService {
+  private cache = new Map<string, CartItemProps>();
+
   constructor(private readonly prisma: PrismaService) {}
 
   async addToCart(userId: number, productId: string) {
@@ -11,6 +17,30 @@ export class CartService {
       update: { quantity: { increment: 1 } },
       create: { userId, productId, quantity: 1 },
     });
+  }
+
+  async decreaseFromCart(userId: number, productId: string) {
+    const item = await this.prisma.cartItem.findUnique({
+      where: { userId_productId: { userId, productId } },
+      select: { quantity: true },
+    });
+
+    if (!item) {
+      throw new Error('Cart item not found');
+    }
+
+    if (item.quantity > 1) {
+      return this.prisma.cartItem.update({
+        where: { userId_productId: { userId, productId } },
+        data: {
+          quantity: { decrement: 1 },
+        },
+      });
+    } else {
+      return this.prisma.cartItem.delete({
+        where: { userId_productId: { userId, productId } },
+      });
+    }
   }
 
   async removeFromCart(userId: number, productId: string) {
@@ -30,31 +60,60 @@ export class CartService {
     });
   }
 
-  async getCart(userId: number) {
-    const items = await this.prisma.cartItem.findMany({
+  async getCart(userId: number): Promise<CartItemProps[]> {
+    const productsIds = await this.prisma.cartItem.findMany({
       where: { userId },
       select: { productId: true, quantity: true },
     });
 
-    const products = await Promise.all(
-      items.map(async (item) => {
-        const res = await fetch(
-          `https://oowcjcmdcfitnnsqfohw.supabase.co/rest/v1/products?select=id,title,price&id=eq.${item.productId}`,
-        );
-        const [product] = await res.json();
-        return { ...product, quantity: item.quantity };
-      }),
-    );
+    const cartList: CartItemProps[] = [];
 
-    return products;
+    for (const item of productsIds) {
+      if (this.cache.has(item.productId)) {
+        console.log('Serving from cache');
+        const cached = this.cache.get(item.productId);
+        if (cached) {
+          cartList.push(cached);
+          continue;
+        }
+        continue;
+      }
+      const res = await fetch(
+        `https://oowcjcmdcfitnnsqfohw.supabase.co/rest/v1/products?id=eq.${item.productId}`,
+        {
+          headers: {
+            apikey: SUPABASE_KEY,
+            Authorization: `Bearer ${SUPABASE_KEY}`,
+          },
+        },
+      );
+      console.log('===> res : ', res);
+      const [data] = await res.json();
+
+      const product: CartItemFullProps = {
+        productId: data.id,
+        productName: data.title,
+        image: data.primary_image,
+        price: data.Price,
+        quantity: item.quantity,
+      };
+
+      this.cache.set(item.productId, product);
+
+      cartList.push(product);
+    }
+
+    console.log('cartList : ', cartList);
+    return cartList;
   }
-
   async getCartItemQuantity(userId: number, productId: string) {
-    return this.prisma.cartItem.findUnique({
+    const quantity = await this.prisma.cartItem.findUnique({
       where: {
         userId_productId: { userId, productId },
       },
       select: { quantity: true },
     });
+    console.log('quantity : ', quantity);
+    return quantity || { quantity: 0 };
   }
 }
