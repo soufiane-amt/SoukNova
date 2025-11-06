6cbc2b4a27a64f8be48c77f381591d83acc9e7ee
Soufiane Amajat <soufianeamt11@gmail.com>
2025-11-06 14:13:59 +0100

    feat : update commenting

--- src/modules/comment/services/comment.service.ts
+++ src/modules/comment/services/comment.service.ts
@@ -7,14 +7,35 @@ export class CommentService {
   constructor(private prisma: PrismaService) {}
 
   async createComment(comment: commentInfoDto) {
-    return this.prisma.comment.create({
+    const review = await this.prisma.comment.create({
       data: {
         userId: comment.userId,
         productId: comment.productId,
         content: comment.content,
         rating: comment.rating,
       },
+      select: {
+        id: true,
+        content: true,
+        rating: true,
+        addedAt: true,
+        user: {
+          select: {
+            firstName: true,
+            lastName: true,
+            image: true,
+          },
+        },
+      },
     });
+
+    return {
+      id: review.id,
+      name: review.user.firstName + ' ' + review.user.lastName,
+      avatar: review.user.image,
+      rate: review.rating,
+      content: review.content,
+    };
   }
 
   async getComments(productId: string) {
@@ -33,8 +54,13 @@ export class CommentService {
           },
         },
       },
+      orderBy: {
+        addedAt: 'desc',
+      },
     });
+
     return comments.map((comment) => ({
+      id: comment.id,
       name: comment.user.firstName + ' ' + comment.user.lastName,
       avatar: comment.user.image,
       rate: comment.rating,
