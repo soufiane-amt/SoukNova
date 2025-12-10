181fc6f69c22f4d920b1740c6683ee7ca8bf41e5
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-10 19:41:41 +0100

    fix : update auth guard

--- backend/src/modules/auth/guards/auth.guard.ts
+++ backend/src/modules/auth/guards/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    const token = this.extractTokenFromCookie(request);
     if (!token) {
       throw new UnauthorizedException();
     }
@@ -34,8 +34,8 @@ export class AuthGuard implements CanActivate {
     return true;
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
+  private extractTokenFromCookie(request: Request): string | undefined {
+    const token = request.cookies?.jwt;
+    return token || undefined;
   }
 }
