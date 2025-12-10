8cb3e4dd3a82117b9e5e40e4b7f6021253486cb4
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-10 19:38:52 +0100

    feat: setting the pipe -  useing cookieParser - update upload folder location

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   const configService = app.get(ConfigService);
   const frontendUrl = configService.get<string>('FRONTEND_URL');
@@ -16,7 +22,11 @@ async function bootstrap() {
     origin: frontendUrl,
     credentials: true,
   });
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+  app.use(cookieParser());
+
+  const uploadsPath = join(process.cwd(), 'uploads');
+
+  app.useStaticAssets(uploadsPath, {
     prefix: '/uploads/',
   });
 
