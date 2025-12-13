349ccc6faa3afd3b222e790e8324e26a08862d65
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-13 19:55:23 +0100

    fix: move fetaching articles to main page

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -9,9 +9,11 @@ import { SiteFooter } from '../../components/layout/SiteFooter';
 import Loader from '../../components/feedback/loader/Loader';
 
 const imageUrl = '/images/blog/ourBlogPage.png';
+const PAGE_SIZE = 16;
 
 function BlogPage() {
-  const [articles, setArticles] = useState([]);
+  const [itemsData, setItemsData] = useState<any>();
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -22,10 +24,30 @@ function BlogPage() {
     });
   }, []);
 
+  useEffect(() => {
+    const fetchPageCatalog = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/article?page=${page}&pageSize=${PAGE_SIZE}`,
+      );
+
+      const data = await response.json();
+      setItemsData(data);
+      setLoading(false);
+    };
+    fetchPageCatalog();
+  }, [page]);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    setPage(value);
+    window.scrollTo({ top: 50, behavior: 'smooth' });
+  };
+
   if (loading) {
     return <Loader />;
   }
-  if (error) return <div>Error: {error}</div>;
 
   return (
     <main>
@@ -37,7 +59,11 @@ function BlogPage() {
             desc="Home ideas and design inspiration"
           />
         </div>
-        <BlogCatalog articles={articles} />
+        <BlogCatalog
+          itemsData={itemsData}
+          page={page}
+          handlePageChange={handlePageChange}
+        />
       </div>
       <SiteFooter />
     </main>
