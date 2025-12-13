8bcf5d37d54a27c80437b55e74048bf567910c58
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-13 19:57:43 +0100

    feat: create Custom paginatio

---
+++ frontend/src/components/ui/CustomPagination.tsx
@@ -0,0 +1,38 @@
+import { Box, Pagination } from '@mui/material';
+
+interface CustomPaginationProps {
+  pagesCount: number;
+  page: number;
+  handlePageChange: (e: React.ChangeEvent<unknown>, v: number) => void;
+}
+function CustomPagination({
+  pagesCount,
+  page,
+  handlePageChange,
+}: CustomPaginationProps) {
+  if (pagesCount <= 1) return null;
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4, mb: 2 }}>
+      <Pagination
+        count={pagesCount}
+        page={page}
+        onChange={handlePageChange}
+        color="primary"
+        shape="rounded"
+        sx={{
+          '& .MuiPaginationItem-root.Mui-selected': {
+            borderRadius: '50%', // fully rounded
+            fontWeight: 'bold', // bold number
+            backgroundColor: '#000', // black background
+            color: '#fff', // white text
+          },
+          '& .MuiPaginationItem-root:hover': {
+            borderRadius: '50%',
+          },
+        }}
+      />
+    </Box>
+  );
+}
+
+export default CustomPagination;
