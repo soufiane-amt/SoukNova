c9ab5a7c07ade413847b23a0a61e3f052efbe970
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-17 20:04:13 +0100

    feat: implement recent products to the front

--- frontend/src/app/home/components/NewArrivalSection.tsx
+++ frontend/src/app/home/components/NewArrivalSection.tsx
@@ -2,12 +2,19 @@
 import { poppins } from '@/layout';
 import CustomButton from '../../../components/buttons/CustomButton';
 import Link from 'next/link';
-import { ProductCard } from '../../../components/cards/ProductCard/ProductCard';
-import { useCart } from '../../../context/CartContext';
+import ProductCard from '../../../components/cards/ProductCard/ProductCard';
+import { useEffect, useState } from 'react';
 
 export default function NewArrivalSection() {
-  const { products } = useCart();
-
+  const [recentProducts, setRecentProducts] = useState([])
+  useEffect(()=>{
+    const fetchRecentProduct = async()=>{
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/product/recent`);
+      const data = await res.json()
+      setRecentProducts(data)
+    }
+    fetchRecentProduct()
+  }, [])
   return (
     <section aria-labelledby="new-arrivals" className="my-12">
       <div className="flex justify-between" data-aos="fade-up">
@@ -21,18 +28,18 @@ export default function NewArrivalSection() {
         </div>
       </div>
       <div className="flex overflow-x-auto overflow-y-hidden space-x-6 py-10 custom-scrollbar">
-        {products.map((item, index) => (
+        {recentProducts.map((item, index) => (
           <div data-aos="fade-up" data-aos-delay={index * 100} key={index}>
             <Link href={`/product/${item.id}`}>
               <ProductCard
                 productId={item.id}
                 productName={item.title}
                 currentPrice={item.price}
-                originalPrice={item.originalPrice}
+                originalPrice={item.price}
                 discountPercentage={item.discount}
                 rating={item.rate}
                 image={item.primary_image}
-                date={Date().toString()}
+                date={item.date}
               />
             </Link>
           </div>
