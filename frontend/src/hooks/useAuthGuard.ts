92c74cafff5a4c7a4e69ad3c3d1a6f45c9e3025e
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-30 19:14:43 +0100

    fix: add "use client" for account layout

---
+++ frontend/src/hooks/useAuthGuard.ts
@@ -0,0 +1,18 @@
+// hooks/useAuthGuard.ts
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export function useAuthGuard() {
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/auth/verify-token`, {
+      credentials: 'include',
+    }).then(res => {
+      if (!res.ok) {
+        router.replace('/auth/signin');
+      }
+    });
+  }, []);
+}
