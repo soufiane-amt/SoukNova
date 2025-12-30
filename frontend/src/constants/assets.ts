89b660f7e58974d3210ec5635a5b91000c7aaf98
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-30 17:24:31 +0100

    feat: add new constants

--- frontend/src/constants/assets.ts
+++ frontend/src/constants/assets.ts
@@ -2,6 +2,7 @@ export const DEFAULT_USER_IMAGE = '/images/myAccount/default-user.png';
 export const AUTH_BACKGROUND_IMAGE = '/images/backgrounds/auth_back_image.png';
 export const PROMO_HIGHLIGHT_IMAGE = '/images/home/fourniture.png';
 export const BLOG_PAGE_IMAGE = '/images/blog/ourBlogPage.png';
+export const ARTICLE_DEFAULT_IMAGE = '/images/blog/article-placeholder.jpg';
 export const SHOP_PAGE_IMAGE = '/images/shop/shopPage.png';
 export const NEWSLETTER_BACKGROUND_IMAGE = '/images/home/newsLetterBack.png';
 export const MAP_LOCATION_IMAGE = '/images/contact/map.png';
