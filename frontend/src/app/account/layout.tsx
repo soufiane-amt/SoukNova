802acacb5654001876cc8002ccf30a84a99bd738
Soufiane Amajat <soufianeamt11@gmail.com>
2025-12-30 19:08:50 +0100

    feat: add useAuthGuard for account pages

--- frontend/src/app/account/layout.tsx
+++ frontend/src/app/account/layout.tsx
@@ -1,11 +1,12 @@
-// app/account/layout.tsx
 import SettingsNavigator from '../../components/ui/Settings/SettingsWrap';
+import { useAuthGuard } from '../../hooks/useAuthGuard';
 
 export default async function AccountLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useAuthGuard();
   return (
     <section className="min-h-screen">
       <SettingsNavigator>{children}</SettingsNavigator>
