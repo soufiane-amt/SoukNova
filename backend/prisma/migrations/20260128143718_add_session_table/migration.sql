97323203be131d211eeb7b9481991d6811bd9544
Soufiane Amajat <soufianeamt11@gmail.com>
2026-02-02 14:42:57 +0100

    feat: add OrderItem, Session, Admin tables

---
+++ backend/prisma/migrations/20260128143718_add_session_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ip" TEXT,
+    "device" TEXT,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
