002795ccf4360fc1ad1554d37ebf77840842249d
Soufiane Amajat <soufianeamt11@gmail.com>
2026-02-21 14:03:37 +0000

    feat: inject RedisModule

--- backend/src/modules/shopping-concierge/shopping-concierge.module.ts
+++ backend/src/modules/shopping-concierge/shopping-concierge.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ShoppingConciergeController } from './controller/shopping-concierge.controller';
 import { ConciergeService } from './service/shopping-concierge.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
+    imports:[RedisModule],
     controllers:[ShoppingConciergeController],
     providers:[ConciergeService, PrismaService]
 })
